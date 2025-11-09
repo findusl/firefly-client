6b1a9b1922b1799b614362e33c4c75f1cee45f49
Sebasitan <dev@findusl.de>
2025-11-09 11:51:37 +0100

    Remove unused Greeting and Platform files

--- composeApp/src/jvmMain/kotlin/de/lehrbaum/firefly/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package de.lehrbaum.firefly
-
-class JvmPlatform : Platform {
-	override val name: String = "JVM ${System.getProperty("os.name") ?: "Unknown OS"}"
-}
-
-actual fun getPlatform(): Platform = JvmPlatform()
