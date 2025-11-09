e5da86e65fbf51624f5ef35caae7adc73dce09ee
Sebasitan <dev@findusl.de>
2025-11-09 11:54:52 +0100

    Adjust to match the compose sample app

---
+++ composeApp/src/wasmJsMain/kotlin/de/lehrbaum/firefly/main.kt
@@ -0,0 +1,9 @@
+package de.lehrbaum.firefly
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+	ComposeViewport { App() }
+}
