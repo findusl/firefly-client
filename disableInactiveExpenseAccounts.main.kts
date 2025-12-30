d179fc624783dc9695b233fe0da779b896bfed90
Sebastian <dev@findusl.de>
2025-12-30 11:54:02 +0100

    Add missing boolean import for expense script

--- disableInactiveExpenseAccounts.main.kts
+++ disableInactiveExpenseAccounts.main.kts
@@ -22,6 +22,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.booleanOrNull
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.intOrNull
